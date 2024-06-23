56c720b99cf9fb023995d9fa54a9cb8c3cd9bb23
NikhilNataraj <nikhil.nataraj@gmail.com>
2024-06-23 14:37:36 +0530

    Updating config files for chrome

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Install dependencies
+# Update package lists and install necessary packages
 apt-get update
 apt-get install -y wget gnupg unzip
 
-# Install Google Chrome
+# Download and install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
 
-# Install ChromeDriver
+# Ensure Google Chrome is available in the PATH
+ln -s /usr/bin/google-chrome /usr/local/bin/google-chrome
+
+# Download and install ChromeDriver
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
