6d970a9791835c2f61a37b90681beeea38057da0
NikhilNataraj <nikhil.nataraj@gmail.com>
2024-06-28 11:08:48 +0530

    Updating config files for chrome 5

--- ht.py
+++ ht.py
@@ -1,26 +1,22 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import time
 
 
 def get_ht_links():
     HT_URL = "https://www.hindustantimes.com/editorials"
 
-    # Set up Chrome options for headless mode
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-gpu')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = os.getenv("CHROME_BIN")
 
-    # Use webdriver_manager to automatically manage ChromeDriver
-    service = Service(ChromeDriverManager().install())
-
+    service = Service(os.getenv("CHROMEDRIVER_BIN"))
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
@@ -48,16 +44,14 @@ def get_ht_links():
 
 
 def get_article(link):
-    # Set up Chrome options for headless mode
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-gpu')  # Optional but useful
-    chrome_options.add_argument('--window-size=1920x1080')  # Optional but useful
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = os.getenv("CHROME_BIN")
 
-    # Use webdriver_manager to automatically manage ChromeDriver
-    service = Service(ChromeDriverManager().install())
+    service = Service(os.getenv("CHROMEDRIVER_BIN"))
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
--- install_chrome.sh
+++ install_chrome.sh
@@ -2,10 +2,14 @@
 
 # Update and install Chromium and Chromedriver
 apt-get update && apt-get install -y \
-    chromium-driver \
     chromium \
+    chromium-driver \
     curl \
     unzip
+
+# Set environment variables for Chromium and Chromedriver
+export CHROME_BIN=/usr/bin/chromium
+export CHROMEDRIVER_BIN=/usr/bin/chromedriver
 
 # Clean up to reduce image size
 apt-get clean && rm -rf /var/lib/apt/lists/*
