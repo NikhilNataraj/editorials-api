796998498dac43d05f932b0fe45da0fd625ee08e
NikhilNataraj <nikhil.nataraj@gmail.com>
2024-06-30 12:41:16 +0530

    Playwright post install config 2

--- postinstall.py
+++ postinstall.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def install_playwright():
-    subprocess.run(["python", "-m", "playwright", "install"], check=True)
+    subprocess.run(["playwright", "install"], check=True)
 
 
 if __name__ == "__main__":
