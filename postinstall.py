c255664e4a6a0f838b1512a1ca028ff75ed68f67
NikhilNataraj <nikhil.nataraj@gmail.com>
2024-06-30 13:05:17 +0530

    Playwright post install config 4

--- postinstall.py
+++ postinstall.py
@@ -1,5 +1,12 @@
-import subprocess
+from playwright.sync_api import sync_playwright
 
 
 def install_playwright():
-    subprocess.run(["playwright", "install"], check=True)
+    with sync_playwright() as p:
+        p.chromium.launch()
+        p.firefox.launch()
+        p.webkit.launch()
+
+
+if __name__ == "__main__":
+    install_playwright()
